2c8c0b6e7d6ae11dff67766228183096c5c2c10a
Markus Spath <markus@hackr.de>
2022-08-22 20:47:34 +0200

    update android studio, kotlin, gradle, libs

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
@@ -1,11 +1,10 @@
 package com.example.android.guesstheword.screens.score
 
-
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class ScoreViewModelFactory(private val finalScore: Int) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ScoreViewModel::class.java)) {
             return ScoreViewModel(finalScore) as T
         }
